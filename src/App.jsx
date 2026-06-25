93f2f341db0289370d86399a3f605ccd90eabfa6
sirvick-concept <sirvick2000@gmail.com>
2026-06-25 12:51:44 +0100

    i update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
